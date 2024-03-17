4025b3b26ac89c7d1d682d6c00748c6eeb778cb9
DavidChewy <77389417+DavidChewy@users.noreply.github.com>
2024-03-17 01:27:49 +0000

    Refactor deep copy of pricing rules

--- src/main/java/solution/CheckoutSystem.java
+++ src/main/java/solution/CheckoutSystem.java
@@ -6,7 +6,7 @@ import solution.models.SKU;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
+
 
 /**
  * This is a singleton hence there is only one instance of this class.
@@ -29,11 +29,7 @@ public class CheckoutSystem {
 
     public CheckoutTransaction createTransaction(){
         //deep copy of pricing rules, if we want to have all checkout transaction share the pricing rule, we just use pricing ruleS in here
-        //TODO new KataCheckoutTransaction (new hash map<>(pricingrules));
-        return new KataCheckoutTransaction(pricingRules
-                .entrySet()
-                .stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+        return new KataCheckoutTransaction(new HashMap<>(pricingRules));
     }
 
     private static final CheckoutSystem INSTANCE = new CheckoutSystem();
