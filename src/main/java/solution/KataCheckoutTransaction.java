4e4b42a744260ccd2c3f17c95f8aadb08d3d4a95
DavidChewy <77389417+DavidChewy@users.noreply.github.com>
2024-03-17 00:52:34 +0000

    Refactor scan method and optimised getTotalPrice

--- src/main/java/solution/KataCheckoutTransaction.java
+++ src/main/java/solution/KataCheckoutTransaction.java
@@ -3,31 +3,34 @@ package solution;
 import solution.models.PriceQuote;
 import solution.models.SKU;
 import solution.models.SpecialPrice;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class KataCheckoutTransaction implements CheckoutTransaction {
 
     private final Map<SKU, PriceQuote> pricingRules;
-    private final List<SKU> items = new ArrayList<>();
+    private final Map<SKU, Integer> items = new HashMap<>();
 
     public KataCheckoutTransaction(Map<SKU, PriceQuote> pricingRules){
         this.pricingRules = pricingRules;
     }
 
     public void scan(SKU item) {
-        items.add(item);
+        items.compute(item, (k, v) -> {
+            if(v == null) { //scan item - if we don't have that item already return 1
+                return 1;
+            } else {
+                return v + 1;
+            }
+        }); // going to put a key in, return a value, if value is null then there is no value in this key.
     }
 
     /**
      * Need to refactor to make it look better to the eye lmao
      */
     public double getTotalPrice() {
-        return items.stream() // go through item like a for loop
-                .collect(Collectors.groupingBy(item -> item, Collectors.counting()))// group by SKU (like 4A... etc) + counting
+        return items
                 .entrySet() //get the set from items
                 .stream() //go through the new elements that have just been set
                 .map(entry -> { // lambda to point to get the key from items
@@ -36,7 +39,7 @@ public class KataCheckoutTransaction implements CheckoutTransaction {
                     int units = entry.getValue().intValue(); // set new field units and assigned value
                     if (priceQuote.getSpecialPrice().isPresent()) { //see if the quote has the special price
                         SpecialPrice specialPrice = priceQuote.getSpecialPrice().get(); //get the special price from SpecialPrice class
-                        int specialApplied = units / specialPrice.getUnitThreshold(); //see the amount of units divdide the specialprice threshold to see how many times you get the special price
+                        int specialApplied = units / specialPrice.getUnitThreshold(); //see the amount of units divides the specialprice threshold to see how many times you get the special price
                         units -= specialApplied * specialPrice.getUnitThreshold(); // result subtracted from units to reflect the units have accounted for or discounted due to the special price
                         totalItemPrice += specialApplied * specialPrice.getPrice(); // calculates the total price for the units that qualify for the special price
                     };
@@ -45,7 +48,5 @@ public class KataCheckoutTransaction implements CheckoutTransaction {
                 })
                 .reduce(Double::sum)
                 .get();
-
-
     }
 }
